09ad92a1da7454da1d52c5098561a9d47c11d965
Cho Youngwoo <cyw5066@naver.com>
2026-09-03 16:46:20 +0900

    Fix ownership and permissions for log and storage directories in entrypoint script

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,8 +2,8 @@
 set -eu
 
 mkdir -p /var/log/mft /var/www/storage
-chown -R www-data:www-data /var/www/storage
-chmod -R 775 /var/www/storage
+chown -R www-data:www-data /var/log/mft /var/www/storage
+chmod -R 775 /var/log/mft /var/www/storage
 
 # rsyslog creates /dev/log for PHP's syslog() calls.
 rsyslogd
